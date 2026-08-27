b220bee95b1691739c54a95796e2123e8cdc0820
Christian P. <105538525+P-Christian@users.noreply.github.com>
2026-08-27 10:54:54 +0800

    Update update-scope.ts

--- src/modules/manufacturing-management/financial-management/cash-issuance/utils/update-scope.ts
+++ src/modules/manufacturing-management/financial-management/cash-issuance/utils/update-scope.ts
@@ -1,4 +1,4 @@
-export const isPaymentAllocationScope = (saveScope?: string) => saveScope === "payment";
+export const isPaymentAllocationScope = (saveScope?: string) => saveScope === "RELEASING_PAYMENT";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const resolveDisbursementUpdateStatus = (currentStatus: string, saveScope?: string, isHeaderOrPayableModified?: boolean) => {
     return currentStatus;
