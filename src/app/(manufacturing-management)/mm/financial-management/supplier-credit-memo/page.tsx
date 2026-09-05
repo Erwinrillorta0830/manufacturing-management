ad1cee4a38121e3b8c99c0a9968249850d63a055
Christian P. <105538525+P-Christian@users.noreply.github.com>
2026-09-05 09:19:39 +0800

    update: Breadcrumbs correction

--- src/app/(manufacturing-management)/mm/financial-management/supplier-credit-memo/page.tsx
+++ src/app/(manufacturing-management)/mm/financial-management/supplier-credit-memo/page.tsx
@@ -95,11 +95,11 @@ export default async function Page() {
                         <Breadcrumb>
                             <BreadcrumbList className="min-w-0 overflow-hidden">
                                 <BreadcrumbItem className="hidden md:block shrink-0">
-                                    <BreadcrumbLink href="#">FM</BreadcrumbLink>
+                                    <BreadcrumbLink href="/mm">Manufacturing</BreadcrumbLink>
                                 </BreadcrumbItem>
                                 <BreadcrumbSeparator className="hidden md:block shrink-0" />
                                 <BreadcrumbItem className="hidden md:block shrink-0">
-                                    <BreadcrumbLink href="#">Accounting</BreadcrumbLink>
+                                    <BreadcrumbLink href="/mm/financial-management">Financial Management</BreadcrumbLink>
                                 </BreadcrumbItem>
                                 <BreadcrumbSeparator className="hidden md:block shrink-0" />
                                 <BreadcrumbItem className="min-w-0 overflow-hidden">
